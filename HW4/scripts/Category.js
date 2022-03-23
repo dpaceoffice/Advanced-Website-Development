c1d08369a81835ffd89078f0045a3523adc01fd1
dpaceoffice <dpaceoffice@gmail.com>
2022-03-23 14:07:13 -0500

    Formatting, Completely documented, handles removing products, updates prices to show US format

--- HW4/scripts/Category.js
+++ HW4/scripts/Category.js
@@ -1,30 +1,54 @@
 /**
  * Category Object
  */
- export default class Category {
+export default class Category {
+  #type; //String describing category
+  #products; //Array of products owned by category
+  #htmlElement; //String containing category html
+  #category_id; //int of categories id
 
-    #type;//String describing category
-    #products;//Array of products owned by category
-    #htmlElement;
-    #category_id;
+  /**
+   * Initalize the object
+   * @param {int} category_id
+   * @param {String} type
+   * @param {Array<Product>} products
+   */
+  constructor(category_id, type, products) {
+    this.#category_id = category_id;
+    this.#type = type;
+    this.#products = products;
+    this.#htmlElement =
+      `<li id="cat_id-${category_id}" class="btn list-group-item" onclick='setActiveCat(${category_id})'>` +
+      this.getType() +
+      `</li>`;
+  }
 
-    constructor(category_id, type, products) {
-        this.#category_id = category_id;
-        this.#type = type;
-        this.#products = products;
-        this.#htmlElement = `<li id="cat_id-${category_id}" class="btn list-group-item" onclick='setActiveCat(${category_id})'>`+this.getType()+`</li>`;
-    }
-
-    getType() {
-        return this.#type;
-    }
-    getProducts() {
-        return this.#products;
-    }
-    getHtmlElement() {
-        return this.#htmlElement;
-    }
-    getID() {
-        return this.#category_id;
-    }
+  /**
+   * Category string name
+   * @returns The category name
+   */
+  getType() {
+    return this.#type;
+  }
+  /**
+   * array [] type of product objects
+   * @returns Array of products in category
+   */
+  getProducts() {
+    return this.#products;
+  }
+  /**
+   * String type
+   * @returns HTML element string
+   */
+  getHtmlElement() {
+    return this.#htmlElement;
+  }
+  /**
+   * int type most important for normalization
+   * @returns Category id
+   */
+  getID() {
+    return this.#category_id;
+  }
 }
--- HW4/scripts/Customer.js
+++ HW4/scripts/Customer.js
@@ -1,17 +1,45 @@
 /**
  * Customer Object
  */
- export default class Customer {
-
-    #customerID;//INT ID of customer
-    #name;//Name of customer
-    #email;//Email of customer
-    #carts;//Array of shopping carts customer has
-
-    constructor(customerID, name, email, carts) {
-        this.#customerID = customerID;
-        this.#name = name;
-        this.#email = email;
-        this.#carts = carts;
-    }
+export default class Customer {
+  #customerID; //INT ID of customer
+  #name; //Name of customer
+  #email; //Email of customer
+  #carts; //Array of shopping carts customer has
+  #curCart = -1; //Current ID of active cart being used
+  /**
+   * Create the object
+   * @param {int} customerID
+   * @param {String} name
+   * @param {String} email
+   * @param {Array<Cart>} carts
+   */
+  constructor(customerID, name, email, carts) {
+    this.#customerID = customerID;
+    this.#name = name;
+    this.#email = email;
+    this.#carts = carts;
+    this.#curCart = 0;
+  }
+  /**
+   * Gets the cart's id
+   * @returns Active cart id
+   */
+  getActiveCart() {
+    return this.#carts[this.#curCart];
+  }
+  /**
+   * Get the private string name
+   * @returns String name of client
+   */
+  getName() {
+    return this.#name;
+  }
+  /**
+   * Prints name of client to welcome them
+   * @returns html element used by client
+   */
+  getHtml() {
+    return `<i class="bi bi-house"></i> Greetings ${this.getName()}!`;
+  }
 }
